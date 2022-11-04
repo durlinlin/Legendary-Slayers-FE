04db6eff9eadc6fa547b22773c40468bbbe9316a
Victor Apaez <vic.apaez@gmail.com>
2022-11-04 15:02:59 -0400

    created nav bar with img and ul

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="nav-container">
+      <img
+        src="https://media.discordapp.net/attachments/573166469525798922/1038163144746467408/unknown.png?width=1562&height=983"
+        alt="League of legends logo"
+      />
+      <ul>
+        <Link to="/champions">All Champions</Link>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
