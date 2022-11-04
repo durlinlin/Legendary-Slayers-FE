60fcb32a52c2fd2dd48371e8ad18081c115e263b
Lin Lin <linlin@Lins-MacBook-Air.local>
2022-11-04 15:31:22 -0400

    edits on app js file

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
 
 function App() {
   return (
