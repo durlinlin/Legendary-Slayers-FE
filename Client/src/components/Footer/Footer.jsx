7f9211c7ad3ae97e28677675d20e8af9437df3bb
Lin Lin <linlin@Lins-MacBook-Air.local>
2022-11-07 10:31:20 -0500

    created footer component

---
+++ Client/src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer>
+      <div className='github-links'>
+        <a className='github-icons' href="https://github.com/durlinlin"></a>
+        <a className='github-icons' href="https://github.com/durlinlin"></a>
+        <a className='github-icons' href="https://github.com/durlinlin"></a>
+        <a className='github-icons' href="https://github.com/durlinlin"></a>
+        <a className='github-icons' href="https://github.com/durlinlin"></a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
