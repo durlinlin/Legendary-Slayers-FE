c1796f2cac68153386c9673e57667ebfde0e36b1
Lin Lin <linlin@Lins-MacBook-Air.local>
2022-11-07 11:57:24 -0500

    added footer css

--- Client/src/components/Footer/Footer.jsx
+++ Client/src/components/Footer/Footer.jsx
@@ -5,15 +5,18 @@ function Footer() {
   return (
     <footer>
       <div className='github-links'>
-        <a className='github-icons' target="_blank" href="https://github.com/dmpellegrini"></a>
-        <a className='github-icons' target="_blank" href="https://github.com/jeanreye4"></a>
-        <a className='github-icons' target="_blank" href="https://github.com/VictorApaez"></a>
-        <a className='github-icons' target="_blank" href="https://github.com/vvsuji"></a>
-        <a className='github-icons' target="_blank" href="https://github.com/durlinlin"></a>
+        <a className='github-icons' target="_blank" rel="noopener" href="https://github.com/dmpellegrini">
+          <span>Danny P.</span>
+        </a>
+        <a className='github-icons' target="_blank" rel="noopener" href="https://github.com/jeanreye4"></a>
+        <a className='github-icons' target="_blank" rel="noopener" href="https://github.com/VictorApaez"></a>
+        <a className='github-icons' target="_blank" rel="noopener" href="https://github.com/vvsuji"></a>
+        <a className='github-icons' target="_blank" rel="noopener" href="https://github.com/durlinlin"></a>
       </div>
       <div className='footer-icons'>
         <a className='footer-logo' target="_blank" href="https://www.leagueoflegends.com/en-us/"></a>
       </div>
+      <div className='maturity-rating'></div>
     </footer>
   )
 }
