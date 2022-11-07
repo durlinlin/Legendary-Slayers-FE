55e775f8fd561f4ff8a20a459968f056ef3d888d
Lin Lin <linlin@Lins-MacBook-Air.local>
2022-11-07 11:07:55 -0500

    edits on footer

--- Client/src/components/Footer/Footer.jsx
+++ Client/src/components/Footer/Footer.jsx
@@ -5,11 +5,14 @@ function Footer() {
   return (
     <footer>
       <div className='github-links'>
-        <a className='github-icons' href="https://github.com/durlinlin"></a>
-        <a className='github-icons' href="https://github.com/durlinlin"></a>
-        <a className='github-icons' href="https://github.com/durlinlin"></a>
-        <a className='github-icons' href="https://github.com/durlinlin"></a>
-        <a className='github-icons' href="https://github.com/durlinlin"></a>
+        <a className='github-icons' target="_blank" href="https://github.com/dmpellegrini"></a>
+        <a className='github-icons' target="_blank" href="https://github.com/jeanreye4"></a>
+        <a className='github-icons' target="_blank" href="https://github.com/VictorApaez"></a>
+        <a className='github-icons' target="_blank" href="https://github.com/vvsuji"></a>
+        <a className='github-icons' target="_blank" href="https://github.com/durlinlin"></a>
+      </div>
+      <div className='footer-icons'>
+        <a className='footer-logo' target="_blank" href="https://www.leagueoflegends.com/en-us/"></a>
       </div>
     </footer>
   )
