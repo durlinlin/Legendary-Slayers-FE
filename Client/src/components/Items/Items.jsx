96f6d7cdb93233d01bda40f4ee5980eac652c016
Victor Apaez <vic.apaez@gmail.com>
2022-11-07 12:25:42 -0500

    created Items component with an img of every item

--- Client/src/components/Items/Items.jsx
+++ Client/src/components/Items/Items.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import itemsJson from "../../Items.json";
+import "./Items.css";
 function Items() {
-  return <div className="items-container"></div>;
+  return (
+    <div className="wrapper">
+      <div className="items-container">
+        {itemsJson.map((item) => {
+          return (
+            <div className="item-card">
+              <img src={`${item.full_image}`} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Items;
