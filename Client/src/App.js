87138b8e4453a5e51c5c24b44d3e03ae965707e9
Victor Apaez <vic.apaez@gmail.com>
2022-11-07 11:03:35 -0500

    added route to /items

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Characters from "./components/Characters/Characters";
 import CharacterInfo from "./components/CharacterInfo/CharacterInfo";
-
+import Items from "./components/Items/Items";
 function App() {
   return (
     <div className="App">
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/champions" element={<Characters />} />
         <Route path="/champions/:champion_name" element={<CharacterInfo />} />
+        <Route path="/items" element={<Items />} />
       </Routes>
     </div>
   );
