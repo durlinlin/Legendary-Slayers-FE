ad2aab322426e5507b2f08e15538599ae46d8ede
Suji Falcon <suji.falcon@gmail.com>
2022-11-07 14:02:18 -0500

    link to specific champion working

--- Client/src/components/CharacterInfo/CharacterInfo.jsx
+++ Client/src/components/CharacterInfo/CharacterInfo.jsx
@@ -3,15 +3,16 @@ import { useParams } from "react-router-dom";
 import champsJson from "../../champs.json";
 
 function CharacterInfo() {
-  const [champion, setChampion] = useState({});
-  const { champion_name } = useParams();
+	const [champion, setChampion] = useState({});
+	const { champion_name } = useParams();
 
-  useEffect(() => {
-    console.log(champion_name);
-    // api call to individual champion
-  }, [champion_name]);
+	useEffect(() => {
+		console.log(champion_name);
+		fetch(`http://ddragon.leagueoflegends.com/cdn/12.21.1/data/en_US/champion/${champion_name}.json
+    `);
+	}, [champion_name]);
 
-  return <div>CharacterInfo</div>;
+	return <div>CharacterInfo</div>;
 }
 
 export default CharacterInfo;
