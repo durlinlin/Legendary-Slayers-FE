db91dbbf54610ea5511ae01ad8554f94f7f507d5
Victor Apaez <vic.apaez@gmail.com>
2022-11-07 15:55:33 -0500

    created hover effect for items

--- Client/src/components/Items/Items.jsx
+++ Client/src/components/Items/Items.jsx
@@ -1,17 +1,43 @@
 import React from "react";
+import { useState } from "react";
+import { useRef } from "react";
 import itemsJson from "../../Items.json";
 import "./Items.css";
 function Items() {
+  const [hidden, setHidden] = useState(true);
+  const [currentItem, setCurrentItem] = useState(null);
+  let popUp = useRef();
+
+  function showInfo(e) {
+    popUp.current.style.top = `${e.clientY + 30}px`;
+    popUp.current.style.left = `${e.clientX - 100}px`;
+  }
+  const handleEnter = (e) => {
+    // set the current item HERE
+    // use URL to find by name use data- attribute
+    setHidden(false);
+  };
+  const handleLeave = () => setHidden(true);
   return (
     <div className="wrapper">
       <div className="items-container">
-        {itemsJson.map((item) => {
+        {itemsJson.map((item, index) => {
           return (
-            <div className="item-card">
+            <div
+              data-name={item.name}
+              className="item-card"
+              onMouseMove={showInfo}
+              onMouseEnter={handleEnter}
+              onMouseLeave={handleLeave}
+              key={index}
+            >
               <img src={`${item.full_image}`} />
             </div>
           );
         })}
+      </div>
+      <div hidden={hidden} className="pop-up" ref={popUp}>
+        <p>The data will go here</p>
       </div>
     </div>
   );
