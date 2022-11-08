85f04becfb6a922246350d89637816db2f19879b
Lin Lin <linlin@lins-air.myfiosgateway.com>
2022-11-07 19:06:03 -0500

    added search bar to characters

--- Client/src/components/Characters/Characters.jsx
+++ Client/src/components/Characters/Characters.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import charsData from "../../champs.json";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./Characters.css";
 import Footer from "../Footer/Footer";
 
 function Characters() {
+  const [name, setName] = useState("")
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setName(e.target.value)
+  }; 
+
   return (
     <div>
     <div className="character-page-wrapper">
@@ -16,17 +24,27 @@ function Characters() {
           playstyle. Master one, or master them all.
         </p>
       </div>
+        <div className="search-bar">
+          <form>
+            <input
+              type="search"
+              className="select-search-input"
+              placeholder="Search Champions"
+              onChange={handleSearch}>
+              </input>
+        </form>
+        </div>  
       <div className="characters-container">
-        {charsData.map((char) => {
-          return (
+        {charsData.map((char) => char.name.toLowerCase().includes(name.toLocaleLowerCase()) &&
+           (
             <Link to={`/champions/${char.name}`}>
               <div className="character-card">
                 <img src={`${char.image_loading}`} alt="" />
                 <h1>{char.name}</h1>
               </div>
             </Link>
-          );
-        })}
+          )
+        )}
         </div>
       </div>
       <Footer />
