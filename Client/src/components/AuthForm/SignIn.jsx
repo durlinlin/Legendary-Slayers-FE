4eec1dd91efac248b2b3487fea539fbdbfd7976a
Victor Apaez <vic.apaez@gmail.com>
2022-11-07 22:45:30 -0500

    storing password and username in useState hook on input change

--- Client/src/components/AuthForm/SignIn.jsx
+++ Client/src/components/AuthForm/SignIn.jsx
@@ -1,24 +1,36 @@
 import React from "react";
+import { useState } from "react";
 import "./SignIn.css";
 
 function SignIn() {
+  const [password, setPassword] = useState();
+  const [username, setUsername] = useState();
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+  }
   return (
     <div className="signIn-container">
-      <form className="signIn-form">
+      <form className="signIn-form" onSubmit={handleFormSubmit}>
         <h1>Sign In</h1>
 
         <input
           type="text"
+          value={username}
           placeholder="Username"
           name="username"
           id="username"
+          onChange={(e) => setUsername(e.target.value)}
         />
 
         <input
+          value={password}
           type="password"
           placeholder="Password"
           name="password"
           id="password"
+          autoComplete="on"
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <button>Submit</button>
