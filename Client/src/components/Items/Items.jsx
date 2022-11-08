1e707013f6f2f4c0e243672bdfd2548fd04d4fbb
Victor Apaez <vic.apaez@gmail.com>
2022-11-07 21:41:15 -0500

    created hover effect that shows item data

--- Client/src/components/Items/Items.jsx
+++ Client/src/components/Items/Items.jsx
@@ -1,43 +1,44 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRef } from "react";
 import itemsJson from "../../Items.json";
 import "./Items.css";
 function Items() {
-  const [hidden, setHidden] = useState(true);
-  const [currentItem, setCurrentItem] = useState(null);
-  let popUp = useRef();
-
   function showInfo(e) {
-    popUp.current.style.top = `${e.clientY + 30}px`;
-    popUp.current.style.left = `${e.clientX - 100}px`;
+    e.currentTarget.nextElementSibling.style.top = `${e.clientY - 100}px`;
+    e.currentTarget.nextElementSibling.style.left = `${e.clientX + 30}px`;
   }
   const handleEnter = (e) => {
-    // set the current item HERE
-    // use URL to find by name use data- attribute
-    setHidden(false);
+    e.currentTarget.nextElementSibling.classList.remove("hide");
   };
-  const handleLeave = () => setHidden(true);
+  const handleLeave = (e) => {
+    e.currentTarget.nextElementSibling.classList.add("hide");
+  };
   return (
     <div className="wrapper">
       <div className="items-container">
         {itemsJson.map((item, index) => {
           return (
-            <div
-              data-name={item.name}
-              className="item-card"
-              onMouseMove={showInfo}
-              onMouseEnter={handleEnter}
-              onMouseLeave={handleLeave}
-              key={index}
-            >
-              <img src={`${item.full_image}`} />
-            </div>
+            <>
+              <div
+                data-name={item.name}
+                className="item-card"
+                onMouseMove={showInfo}
+                onMouseEnter={handleEnter}
+                onMouseLeave={handleLeave}
+                key={index}
+              >
+                <img src={`${item.full_image}`} />
+              </div>
+              <div className="pop-up hide">
+                <p>{item.name}</p>
+                <p>{item.plaintext}</p>
+                <p>Buy Price: {item.buy_price}</p>
+                <p>Sell Price: {item.sell_price}</p>
+              </div>
+            </>
           );
         })}
-      </div>
-      <div hidden={hidden} className="pop-up" ref={popUp}>
-        <p>The data will go here</p>
       </div>
     </div>
   );
