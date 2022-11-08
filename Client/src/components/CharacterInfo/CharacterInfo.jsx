eaf344eca9ee614d3b393854726c62969aa928d9
Suji Falcon <suji.falcon@gmail.com>
2022-11-08 14:04:11 -0500

    added new Beaufort for LOL font and displayed champ name and title

--- Client/src/components/CharacterInfo/CharacterInfo.jsx
+++ Client/src/components/CharacterInfo/CharacterInfo.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import style from "./CharacterInfo.module.css";
-// import champsJson from "../../champs.json";
 
 function CharacterInfo() {
 	const [champion, setChampion] = useState({});
@@ -12,94 +11,87 @@ function CharacterInfo() {
 			`https://legendary-slayers-be-production.up.railway.app/champions/name/${champion_name}`,
 		)
 			.then((res) => res.json())
-			// .then((res) => console.log(res[0]));
-			.then((res) => setChampion(res[0]))
-			.then(() => console.log(champion));
+			.then((res) => setChampion(res[0]));
 	}, []);
 
-	useEffect(() => {
-		// console.log(champion_name);
-	}, []);
-	console.log(champion);
 	return (
-		champion && <h1 className={style.champname}>{champion.title}</h1>
-		// <div className={style.main}>
-		// 	<div className={style.backgroundSplash}>
-		// 		<img
-		// 			src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion_name}_0.jpg`}
-		// 			className={style.backgroundImg}
-		// 			alt=""
-		// 		/>
-		// 	</div>
-		// 	<div className={style.splashImg}>
-		// 		<img
-		// 			src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion_name}_0.jpg`}
-		// 			id={style.champSplash}
-		// 			alt=""
-		// 		/>
-		// 	</div>
-		// 	<div className={style.descWrapper}>
-		// 		<div className={style.content}>
-		// 			<span className={style.title}>{}</span>
-		// 			<span className={style.name}>{champion_name}</span>
-		// 		</div>
-		// 		<div className={style.info}>
-		// 			<div className={style.roles}></div>
-		// 			<div className={style.description}></div>
-		// 		</div>
-		// <div className={style.optionsList}>
-		// 			<h2 className={style.abilitiesTitle}></h2>
-		// 			<button className={style.abilities}>
-		// 				<span className={style.ability1}>
-		// 					<span className={style.abilityIcon1}></span>
-		// 					<span className={style.abilityIcon1Border}></span>
-		// 				</span>
-		// 				<span className={style.optionLine}></span>
-		// 				<span className={style.optionBullet}>
-		// 					::before
-		// 					<span className={style.selectBullet}></span>
-		// 					::after
-		// 				</span>
-		// 			</button>
-		// 			<button className={style.abilities}>
-		// 				<span className={style.ability2}>
-		// 					<span className={style.abilityIcon2}></span>
-		// 					<span className={style.abilityIcon2Border}></span>
-		// 				</span>
-		// 				<span className={style.optionLine}></span>
-		// 				<span className={style.optionBullet}>
-		// 					::before
-		// 					<span className={style.selectBullet}></span>
-		// 					::after
-		// 				</span>
-		// 			</button>
-		// 			<button className={style.abilities}>
-		// 				<span className={style.ability3}>
-		// 					<span className={style.abilityIcon3}></span>
-		// 					<span className={style.abilityIcon3Border}></span>
-		// 				</span>
-		// 				<span className={style.optionLine}></span>
-		// 				<span className={style.optionBullet}>
-		// 					::before
-		// 					<span className={style.selectBullet}></span>
-		// 					::after
-		// 				</span>
-		// 			</button>
-		// 			<button className={style.abilities}>
-		// 				<span className={style.ability4}>
-		// 					<span className={style.abilityIcon4}></span>
-		// 					<span className={style.abilityIcon4Border}></span>
-		// 				</span>
-		// 				<span className={style.optionLine}></span>
-		// 				<span className={style.optionBullet}>
-		// 					::before
-		// 					<span className={style.selectBullet}></span>
-		// 					::after
-		// 				</span>
-		// 			</button>
-		// 		</div>
-		// 	</div>
-		// </div>
+		<div className={style.main}>
+			<div className={style.backgroundSplash}>
+				<img
+					src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion_name}_0.jpg`}
+					className={style.backgroundImg}
+					alt=""
+				/>
+			</div>
+			<div className={style.splashImg}>
+				<div className={style.content}>
+					<span className={style.title}>{champion.title}</span>
+					<strong className={style.name}>{champion.name}</strong>
+				</div>
+				<img
+					src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion_name}_0.jpg`}
+					id={style.champSplash}
+					alt=""
+				/>
+			</div>
+			<div className={style.descWrapper}>
+				<div className={style.info}>
+					<div className={style.roles}></div>
+					<div className={style.description}></div>
+				</div>
+				<div className={style.optionsList}>
+					<h2 className={style.abilitiesTitle}></h2>
+					<button className={style.abilities}>
+						<span className={style.ability1}>
+							<span className={style.abilityIcon1}></span>
+							<span className={style.abilityIcon1Border}></span>
+						</span>
+						<span className={style.optionLine}></span>
+						<span className={style.optionBullet}>
+							::before
+							<span className={style.selectBullet}></span>
+							::after
+						</span>
+					</button>
+					<button className={style.abilities}>
+						<span className={style.ability2}>
+							<span className={style.abilityIcon2}></span>
+							<span className={style.abilityIcon2Border}></span>
+						</span>
+						<span className={style.optionLine}></span>
+						<span className={style.optionBullet}>
+							::before
+							<span className={style.selectBullet}></span>
+							::after
+						</span>
+					</button>
+					<button className={style.abilities}>
+						<span className={style.ability3}>
+							<span className={style.abilityIcon3}></span>
+							<span className={style.abilityIcon3Border}></span>
+						</span>
+						<span className={style.optionLine}></span>
+						<span className={style.optionBullet}>
+							::before
+							<span className={style.selectBullet}></span>
+							::after
+						</span>
+					</button>
+					<button className={style.abilities}>
+						<span className={style.ability4}>
+							<span className={style.abilityIcon4}></span>
+							<span className={style.abilityIcon4Border}></span>
+						</span>
+						<span className={style.optionLine}></span>
+						<span className={style.optionBullet}>
+							::before
+							<span className={style.selectBullet}></span>
+							::after
+						</span>
+					</button>
+				</div>
+			</div>
+		</div>
 	);
 }
 
