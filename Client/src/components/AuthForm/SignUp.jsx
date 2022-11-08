5f8e39d4300c93b784fbf7a3a11d77ca85e87ee1
Victor Apaez <vic.apaez@gmail.com>
2022-11-08 14:55:59 -0500

    created sign up form

--- Client/src/components/AuthForm/SignUp.jsx
+++ Client/src/components/AuthForm/SignUp.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import "./SignUp.css";
+import { useRef } from "react";
+import { Link } from "react-router-dom";
 
 function SignUp() {
-  return <div className="signUp-container">SignUp</div>;
+  const passwordRef = useRef(null);
+  const usernameRef = useRef(null);
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <div className="signIn-container">
+      <form className="signIn-form" onSubmit={handleFormSubmit}>
+        <h1>Sign Up</h1>
+        <input
+          type="text"
+          ref={usernameRef}
+          placeholder="Username"
+          name="username"
+          id="username"
+        />
+        <input
+          ref={passwordRef}
+          type="password"
+          placeholder="Password"
+          name="password"
+          id="password"
+          autoComplete="on"
+        />
+        <button>Submit</button>
+        <Link to="/signIn">Sign In</Link>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
