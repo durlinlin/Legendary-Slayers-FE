f78a30642ac9ef647fbe0ec78e964b1d3f431f2b
Suji Falcon <suji.falcon@gmail.com>
2022-11-08 10:06:33 -0500

    character background and foreground looking good

--- Client/src/components/CharacterInfo/CharacterInfo.jsx
+++ Client/src/components/CharacterInfo/CharacterInfo.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import style from "./CharacterInfo.module.css";
-import champsJson from "../../champs.json";
+// import champsJson from "../../champs.json";
 
 function CharacterInfo() {
 	// const [champion, setChampion] = useState({});
@@ -15,6 +15,13 @@ function CharacterInfo() {
 
 	return (
 		<div className={style.main}>
+			<div className={style.backgroundSplash}>
+				<img
+					src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion_name}_0.jpg`}
+					className={style.backgroundImg}
+					alt=""
+				/>
+			</div>
 			<div className={style.splashImg}>
 				<img
 					src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion_name}_0.jpg`}
