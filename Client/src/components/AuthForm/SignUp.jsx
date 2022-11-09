2abbaa938ccfec8aab7326684e139dec8780a29f
Victor Apaez <vic.apaez@gmail.com>
2022-11-09 09:22:17 -0500

    updated sign up

--- Client/src/components/AuthForm/SignUp.jsx
+++ Client/src/components/AuthForm/SignUp.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import "./SignUp.css";
 import { useRef } from "react";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+//npm i jwt-decode
 function SignUp() {
   const passwordRef = useRef(null);
   const usernameRef = useRef(null);
+  const navigate = useNavigate();
 
-  function handleFormSubmit(e) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleFormSubmit(e) {
     e.preventDefault();
+    const data = JSON.stringify({
+      userName: username,
+      passWord: password,
+    });
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    fetch(
+      "https://legendary-slayers-be-production.up.railway.app/users/signUp",
+      {
+        method: "POST",
+        headers: myHeaders,
+        body: data,
+      }
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        localStorage.setItem("token", res.token);
+      });
   }
   return (
     <div className="signIn-container">
@@ -17,27 +40,22 @@ function SignUp() {
         <h1>Sign Up</h1>
         <input
           type="text"
-          ref={usernameRef}
+          value={username}
           placeholder="Username"
           name="username"
           id="username"
+          onChange={(e) => setUsername(e.target.value)}
         />
         <input
-          ref={passwordRef}
+          value={password}
           type="password"
           placeholder="Password"
           name="password"
           id="password"
           autoComplete="on"
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <input
-          ref={passwordRef}
-          type="password"
-          placeholder="Confirm Password"
-          name="password"
-          id="password"
-          autoComplete="on"
-        />
+
         <button>Submit</button>
       </form>
     </div>
