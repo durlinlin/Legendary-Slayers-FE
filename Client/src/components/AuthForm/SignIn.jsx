5c64d1a27cf8cb9e1acdc7b5d2d188078afe3918
Victor Apaez <vic.apaez@gmail.com>
2022-11-09 10:17:06 -0500

    added fetch to database to validate username and password

--- Client/src/components/AuthForm/SignIn.jsx
+++ Client/src/components/AuthForm/SignIn.jsx
@@ -1,14 +1,35 @@
 import React from "react";
 import { useRef } from "react";
 import "./SignIn.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function SignIn() {
-  const passwordRef = useRef(null);
-  const usernameRef = useRef(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   function handleFormSubmit(e) {
     e.preventDefault();
+    const data = JSON.stringify({
+      userName: username,
+      passWord: password,
+    });
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    fetch(
+      "https://legendary-slayers-be-production.up.railway.app/users/signIn",
+      {
+        method: "POST",
+        headers: myHeaders,
+        body: data,
+      }
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        localStorage.setItem("token", res.token);
+        navigate("/userProfile");
+      });
   }
   return (
     <div className="signIn-container">
@@ -16,21 +37,25 @@ function SignIn() {
         <h1>Sign In</h1>
         <input
           type="text"
-          ref={usernameRef}
+          value={username}
           placeholder="Username"
           name="username"
           id="username"
+          onChange={(e) => setUsername(e.target.value)}
         />
         <input
-          ref={passwordRef}
+          value={password}
           type="password"
           placeholder="Password"
           name="password"
           id="password"
           autoComplete="on"
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button>Log In</button>
-        <button><Link to="/signUp">Create account</Link></button>
+        <button>
+          <Link to="/signUp">Create account</Link>
+        </button>
       </form>
     </div>
   );
