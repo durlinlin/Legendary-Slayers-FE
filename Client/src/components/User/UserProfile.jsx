3a8d53529deab0d8ca249830bee0063239200512
Victor Apaez <vic.apaez@gmail.com>
2022-11-09 17:02:03 -0500

    idk

--- Client/src/components/User/UserProfile.jsx
+++ Client/src/components/User/UserProfile.jsx
@@ -3,63 +3,69 @@ import { useState, useEffect } from "react";
 import "./UserProfile.css";
 
 function UserProfile(props) {
-  const [charsIcon, setCharsIcon] = useState([])
-  const [itemsData, setItemsData] = useState([])
-  const [userName, setUserName] = useState([])
-  const [charsFav, setCharsFav] = useState([])
-  const [itemsFav, setItemsFav] = useState([])
-  localStorage.getItem("token")
-  
+  const [charsIcon, setCharsIcon] = useState([]);
+  const [itemsData, setItemsData] = useState([]);
+  const [userName, setUserName] = useState([]);
+  const [charsFav, setCharsFav] = useState([]);
+  const [itemsFav, setItemsFav] = useState([]);
+  localStorage.getItem("token");
   let fetchItemsData = () => {
     fetch("https://legendary-slayers-be-production.up.railway.app/items/all")
       .then((res) => res.json())
       .then((data) => {
-        setItemsData(data)
-      })
-  }
+        setItemsData(data);
+      });
+  };
 
   let fetchData = () => {
-    fetch("https://legendary-slayers-be-production.up.railway.app/champions/all")
+    fetch(
+      "https://legendary-slayers-be-production.up.railway.app/champions/all"
+    )
       .then((res) => res.json())
       .then((data) => {
-        setCharsIcon(data)
-      })
-  }
-  
+        setCharsIcon(data);
+      });
+  };
+
   let fetchUserData = () => {
-    fetch("https://legendary-slayers-be-production.up.railway.app/users/name/victor")
+    fetch(
+      "https://legendary-slayers-be-production.up.railway.app/users/name/victor"
+    )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data[0].userName, data[0].favCharacters)
-        setUserName(data[0].userName)
-        setCharsFav(data[0].favCharacters)
-        setItemsFav(data[0].favItems)
-      })
-  }
+        setUserName(data[0].userName);
+        setCharsFav(data[0].favCharacters);
+        setItemsFav(data[0].favItems);
+      });
+  };
 
   let addItemFav = (event) => {
-    fetch(`https://legendary-slayers-be-production.up.railway.app/users/name/victor/items/${event.target.id}`,
-    {
-      method: "PUT"
-    })
-  }
+    fetch(
+      `https://legendary-slayers-be-production.up.railway.app/users/name/victor/items/${event.target.id}`,
+      {
+        method: "PUT",
+      }
+    );
+  };
 
   let addCharFav = (event) => {
-    fetch(`https://legendary-slayers-be-production.up.railway.app/users/name/victor/champions/${event.target.id}`,
-    {
-      method: "PUT"
-    })
-  }
+    fetch(
+      `https://legendary-slayers-be-production.up.railway.app/users/name/victor/champions/${event.target.id}`,
+      {
+        method: "PUT",
+      }
+    );
+  };
 
   useEffect(() => {
-    fetchData()
-    fetchItemsData()
-  }, [])
+    fetchData();
+    fetchItemsData();
+  }, []);
 
   useEffect(() => {
-    fetchUserData()
-  }, [])
-  
+    fetchUserData();
+  }, []);
+
   return (
     <div className="user-wrapper">
       <div className="user-title-section">
@@ -74,48 +80,71 @@ function UserProfile(props) {
       </div>
 
       <div className="user-container">
-          <div className="fav-container">
-            <div className="fav-champs">
-              <h3>Champions</h3>
-              <div className="champIcons">
+        <div className="fav-container">
+          <div className="fav-champs">
+            <h3>Champions</h3>
+            <div className="champIcons">
               {charsIcon.map((data) => (
-                <button id={data._id} onClick={addCharFav}className="eachIcon"style={{ backgroundImage: `url("${data.image_square}")` }}>{data.name}</button>
+                <button
+                  id={data._id}
+                  onClick={addCharFav}
+                  className="eachIcon"
+                  style={{ backgroundImage: `url("${data.image_square}")` }}
+                >
+                  {data.name}
+                </button>
               ))}
-              </div>
             </div>
-            <div className="fav-items">
-              <h3>Items</h3>
-              <div className="champIcons">
+          </div>
+          <div className="fav-items">
+            <h3>Items</h3>
+            <div className="champIcons">
               {itemsData.map((data) => (
-                <button id={data._id} onClick={addItemFav}className="eachIcon"style={{ backgroundImage: `url("${data.full_image}")` }}>{data.name}</button>
+                <button
+                  id={data._id}
+                  onClick={addItemFav}
+                  className="eachIcon"
+                  style={{ backgroundImage: `url("${data.full_image}")` }}
+                >
+                  {data.name}
+                </button>
               ))}
-              </div>
             </div>
           </div>
-          <div className="user-fav">
-            <div className="userFav favChamps">
-              <h3>Favorite Champions</h3>
-              <div className="champIcons">
+        </div>
+        <div className="user-fav">
+          <div className="userFav favChamps">
+            <h3>Favorite Champions</h3>
+            <div className="champIcons">
               {charsFav.map((data) => (
-                <button className="eachIcon"style={{ backgroundImage: `url("${data.image_square}")` }}>{data.name}</button>
+                <button
+                  className="eachIcon"
+                  style={{ backgroundImage: `url("${data.image_square}")` }}
+                >
+                  {data.name}
+                </button>
               ))}
-              </div>
-            </div>
-            <div className="userFav favItems">
-              <h3>Favorite Items</h3>
-              <div className="champIcons">
-              {itemsFav.map((data) => (
-                <button className="eachIcon"style={{ backgroundImage: `url("${data.full_image}")` }}>{data.name}</button>
-              ))}
-              </div>
             </div>
           </div>
-          {/* <div className="user-teams">
+          <div className="userFav favItems">
+            <h3>Favorite Items</h3>
+            <div className="champIcons">
+              {itemsFav.map((data) => (
+                <button
+                  className="eachIcon"
+                  style={{ backgroundImage: `url("${data.full_image}")` }}
+                >
+                  {data.name}
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
+        {/* <div className="user-teams">
             <div className="userTeam TeamOne"><h3>Team 1</h3></div>
             <div className="userTeam TeamTwo"><h3>Team 2</h3></div>
             <div className="userTeam TeamThree"><h3>Team 1</h3></div>
           </div> */}
-        
       </div>
     </div>
   );
